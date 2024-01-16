e4344272061dc6bd49ec19510f7584c117d0a0c8
Elias Gill <eliasgill42@gmail.com>
2024-01-16 13:47:17 -0300

    feat(git_saver): Autogenerated commit (mar 16 ene 2024 13:47:17 -03)

--- basic_django/urls.py
+++ basic_django/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import HomeView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", HomeView.as_view(), name="home"),
 ]
